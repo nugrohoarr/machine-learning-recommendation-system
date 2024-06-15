d02fd530c5659285de4deb4fa982fad7d994072e
nugrohoarr <mit.shuki23@gmail.com>
2024-06-15 13:16:56 +0700

    feat: add hello world on app.py

--- app.py
+++ app.py
@@ -71,5 +71,9 @@ def recommend_near():
 
     return jsonify(result)
 
+@app.route('/', methods=['GET'])
+def index():
+    return 'Hello, World!'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
