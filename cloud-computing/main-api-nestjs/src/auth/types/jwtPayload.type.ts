5b45794ad0170b2820e326a74629b77a28cfe11c
inastsabitaah <127664527+inastsabitaah@users.noreply.github.com>
2024-06-18 17:35:31 +0700

    Delete cloud-computing directory

--- cloud-computing/main-api-nestjs/src/auth/types/jwtPayload.type.ts
+++
@@ -1,5 +0,0 @@
-export type JwtPayload = {
-  id: string;
-  name: string;
-  email: string;
-};
--- cloud-computing/main-api-nestjs/src/laundry/dto/update-laundry.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateLaundryDto } from './create-laundry.dto';
-
-export class UpdateLaundryDto extends PartialType(CreateLaundryDto) {}
